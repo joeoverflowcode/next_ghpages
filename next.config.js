ef62168ed9f18ac9cdf772808a7e842ca3c96d75
Joe Aguado <aguado.joe@gmail.com>
2024-05-15 15:14:07 -0500

    added: next.conf.js basePath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/next_ghpages",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
   };
